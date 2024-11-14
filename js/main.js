5d5af3e6b0ac665ded20d098d944c3fe9c2e8e9f
Your Name <darkcreationsl98@gmail.com>
2024-11-14 22:30:51 +0530

    login done

--- js/main.js
+++ js/main.js
@@ -11,13 +11,40 @@ let userArr=[];
 
 /*user management  start*/
     const createUser=()=>{
-        let createdUser=new User('','','');
-        let existsUserData=userArr.find(e=>e.email=='');
+        const fullName=$('#full-Name').val();
+        const email=$('#register-email').val();
+        const password=$('#register-password').val();
+
+
+        let createdUser=new User(fullName,email,password);
+        let existsUserData=userArr.find(e=>e.email==email);
         if (existsUserData){
             alert('User already registered!!!');
             return;
         }
         userArr.push(createdUser);
+        console.log(userArr);
+
+    }
+
+    const login=()=>{
+            const email=$('#email').val();
+            const password=$('#password').val();
+        let existsUserData=userArr.find(e=>e.email==email);
+        if (existsUserData){
+            if (existsUserData.password===password){
+                //-.navigate to the dashboard
+                console.log('user credintials match');
+            }else {
+                alert('password is wrong please enter correct password!!!!');
+                return;
+            }
+
+
+        }else{
+            alert('User email not found!!!');
+        }
+
 
     }
 /*user management  end*/
