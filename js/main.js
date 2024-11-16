3aa27caee36e7a647b0b16b42dd84f77a4ce3a7f
Your Name <darkcreationsl98@gmail.com>
2024-11-16 22:02:18 +0530

    create cus

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-
+let userArr=[];
+let customerArr=[];
 
 class User{
     constructor(fullName,email,password) {
@@ -7,7 +8,16 @@ class User{
         this.password=password;
     }
 }
-let userArr=[];
+
+class Customer{
+    constructor(name,address,salary,nic) {
+        this.name=name;
+        this.address=address;
+        this.salary=salary;
+        this.nic=nic;
+    }
+}
+
 
 /*user management  start*/
     const createUser=()=>{
@@ -28,12 +38,16 @@ let userArr=[];
     }
 
     const login=()=>{
+
             const email=$('#email').val();
             const password=$('#password').val();
         let existsUserData=userArr.find(e=>e.email==email);
         if (existsUserData){
             if (existsUserData.password===password){
-                //-.navigate to the dashboard
+                $('#new-customer-context').show();
+                $('#login-context').hide();
+                $('#sign-up-context').hide();
+
                 console.log('user credintials match');
             }else {
                 alert('password is wrong please enter correct password!!!!');
@@ -48,3 +62,27 @@ let userArr=[];
 
     }
 /*user management  end*/
+
+
+
+/*customer management start*/
+const createCustomer=()=>{
+    const cusomer=new Customer(
+            $('#name').val(),
+            $('#address').val(),
+            parseInt($('#salary').val()) ,
+            $('#nic').val()
+        );
+    let existsCustomer=customerArr.find(cus=>cus.nic===cusomer.nic);
+    if (existsCustomer){
+        console.log('Client NIC already exists!!!');
+        return;
+    }
+
+    customerArr.push(cusomer);
+    console.log(customerArr);
+    $('#customer-form').trigger('reset');
+
+}
+
+/*customer management end*/
